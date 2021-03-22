a5491b0337cb19827a4091d36e409f537809c1bd
Jose Carlos Magno <zemagno@gmail.com>
2021-03-22 17:29:03 -0300

    Mudança de test-series

--- ac-test-run.sh
+++ ac-test-run.sh
@@ -10,7 +10,8 @@ UITEST_PATH='UITestDemo.UITest'
 eval MSBuild $APPCENTER_SOURCE_DIRECTORY/$UITEST_PATH -v:q 
 
 # Upload tests
-App_Center_Test_Command='appcenter test run uitest --app $TEAM_APP --devices $DEVICE_SET --app-path $APP_PACKAGE  --test-series "gh-$APPCENTER_BRANCH" --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/$UITEST_PATH/bin/Debug --async --token $API_TOKEN --uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/packages/Xamarin.UITest.*/tools'
+App_Center_Test_Command='appcenter test run uitest --app $TEAM_APP --devices $DEVICE_SET --app-path $APP_PACKAGE  --test-series "$APPCENTER_BRANCH" --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/$UITEST_PATH/bin/Debug --async --token $API_TOKEN --uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/packages/Xamarin.UITest.*/tools'
+# App_Center_Test_Command='appcenter test run uitest --app $TEAM_APP --devices $DEVICE_SET --app-path $APP_PACKAGE  --test-series "gh-$APPCENTER_BRANCH" --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/$UITEST_PATH/bin/Debug --async --token $API_TOKEN --uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/packages/Xamarin.UITest.*/tools'
 
 echo $App_Center_Test_Command
 eval $App_Center_Test_Command
