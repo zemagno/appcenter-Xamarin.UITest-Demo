8acb6ca936cbf1966df960c106bfa2b8935c64be
Jose Carlos Magno <zemagno@gmail.com>
2021-03-24 09:24:45 -0300

    Teste #25

--- Droid/appcenter-post-build.sh
+++ Droid/appcenter-post-build.sh
@@ -11,7 +11,7 @@
 # $DEVICE_SET
 
 #echo "Start Test upload script (ac-test-run.sh)"
-sh ../ac-test-run.sh
+# sh ../ac-test-run.sh
 #echo "Finish Test upload script (ac-test-run.sh)"
 
 echo "Start Distribute script (ac-distribute.sh)"
